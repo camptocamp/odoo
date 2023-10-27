b25afb4e126009c3faa97777452b8410cfaa20a0
Denis Ledoux <dle@odoo.com>
2023-10-27 16:40:22 +0200

    core: disable x2many manipulation as sudo for sensitive models
    This revision adds a flag
    `_allow_sudo_commands` to which models can opt-in
    to protect themselves against malicious
    manipulation of one2many or many2many fields
    through an environment using `sudo` or a more priviledged user.
    
    Flagging a model `_allow_sudo_commands = False`
    disable `sudo` and `with_user(...)`
    when manipulating a one2many or many2many
    field targeting this model.
    
    task-3695103

--- odoo/addons/base/models/ir_actions.py
+++ odoo/addons/base/models/ir_actions.py
@@ -25,6 +25,7 @@ class IrActions(models.Model):
     _description = 'Actions'
     _table = 'ir_actions'
     _order = 'name'
+    _allow_sudo_commands = False
 
     name = fields.Char(required=True)
     type = fields.Char(string='Action Type', required=True)
@@ -125,6 +126,7 @@ class IrActionsActWindow(models.Model):
     _inherit = 'ir.actions.actions'
     _sequence = 'ir_actions_id_seq'
     _order = 'name'
+    _allow_sudo_commands = False
 
     @api.constrains('res_model', 'binding_model_id')
     def _check_model(self):
@@ -257,6 +259,7 @@ class IrActionsActWindowView(models.Model):
     _table = 'ir_act_window_view'
     _rec_name = 'view_id'
     _order = 'sequence,id'
+    _allow_sudo_commands = False
 
     sequence = fields.Integer()
     view_id = fields.Many2one('ir.ui.view', string='View')
@@ -276,6 +279,7 @@ class IrActionsActWindowclose(models.Model):
     _description = 'Action Window Close'
     _inherit = 'ir.actions.actions'
     _table = 'ir_actions'
+    _allow_sudo_commands = False
 
     type = fields.Char(default='ir.actions.act_window_close')
 
@@ -287,6 +291,7 @@ class IrActionsActUrl(models.Model):
     _inherit = 'ir.actions.actions'
     _sequence = 'ir_actions_id_seq'
     _order = 'name'
+    _allow_sudo_commands = False
 
     name = fields.Char(string='Action Name', translate=True)
     type = fields.Char(default='ir.actions.act_url')
@@ -320,6 +325,7 @@ class IrActionsServer(models.Model):
     _inherit = 'ir.actions.actions'
     _sequence = 'ir_actions_id_seq'
     _order = 'sequence,name'
+    _allow_sudo_commands = False
 
     DEFAULT_PYTHON_CODE = """# Available variables:
 #  - env: Odoo Environment on which the action is triggered
@@ -566,6 +572,7 @@ class IrServerObjectLines(models.Model):
     _name = 'ir.server.object.lines'
     _description = 'Server Action value mapping'
     _sequence = 'ir_actions_id_seq'
+    _allow_sudo_commands = False
 
     server_id = fields.Many2one('ir.actions.server', string='Related Server Action', ondelete='cascade')
     col1 = fields.Many2one('ir.model.fields', string='Field', required=True)
@@ -637,6 +644,7 @@ class IrActionsTodo(models.Model):
     _name = 'ir.actions.todo'
     _description = "Configuration Wizards"
     _order = "sequence, id"
+    _allow_sudo_commands = False
 
     action_id = fields.Many2one('ir.actions.actions', string='Action', required=True, index=True)
     sequence = fields.Integer(default=10)
@@ -725,6 +733,7 @@ class IrActionsActClient(models.Model):
     _table = 'ir_act_client'
     _sequence = 'ir_actions_id_seq'
     _order = 'name'
+    _allow_sudo_commands = False
 
     name = fields.Char(string='Action Name', translate=True)
     type = fields.Char(default='ir.actions.client')
