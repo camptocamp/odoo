c656dd793eb8892f3678c23f7cd03b3839b50f10
Denis Ledoux <dle@odoo.com>
2023-10-27 16:40:22 +0200

    core: disable x2many manipulation as sudo for sensitive models
    This revision adds a flag
    `_allow_sudo_commands` to which models can opt-in
    to protect themselves against malicious
    manipulation of one2many or many2many fields
    through an environment using `sudo` or a more priviledged user.
    
    Flagging a model `_allow_sudo_commands = False`
    disable `sudo` and `with_user(...)`
    when manipulating a one2many or many2many
    field targeting this model.
    
    task-3695103

--- odoo/addons/base/models/ir_module.py
+++ odoo/addons/base/models/ir_module.py
@@ -78,6 +78,7 @@ class ModuleCategory(models.Model):
     _name = "ir.module.category"
     _description = "Application"
     _order = 'name'
+    _allow_sudo_commands = False
 
     @api.depends('module_ids')
     def _compute_module_nr(self):
@@ -154,6 +155,7 @@ class Module(models.Model):
     _rec_name = "shortdesc"
     _description = "Module"
     _order = 'application desc,sequence,name'
+    _allow_sudo_commands = False
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
@@ -991,6 +993,7 @@ DEP_STATES = STATES + [('unknown', 'Unknown')]
 class ModuleDependency(models.Model):
     _name = "ir.module.module.dependency"
     _description = "Module dependency"
+    _allow_sudo_commands = False
 
     # the dependency name
     name = fields.Char(index=True)
@@ -1033,6 +1036,7 @@ class ModuleDependency(models.Model):
 class ModuleExclusion(models.Model):
     _name = "ir.module.module.exclusion"
     _description = "Module exclusion"
+    _allow_sudo_commands = False
 
     # the exclusion name
     name = fields.Char(index=True)
