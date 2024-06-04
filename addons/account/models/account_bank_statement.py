8791956c35ab8f11aa57397016692d1e218672c4
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-06-04 15:44:17 +0200

    account: Allow skipping bank account creation on reconciliation
    This commit introduces system parameter to skip the creation of bank
    account in the reconciliation of bank statements.
    
    The issue it can solve happens when 2 different commercial entities
    use the same paying partner (ie a partner that is not a subcontact)
    to pay their invoices.
    
    When an invoice is paid by the paying partner, Odoo will store the
    account number that was used for the transfer on account.bank.statement.line.
    When this statement line is reconciled with an invoice, if the bank
    account was not stored on the partner previously, a res.partner.bank will be
    created automatically.
    When another payment is coming from the same bank account, Odoo will
    then select the partner linked to the bank account that it did store
    previously, even if the payment was for an invoice linked to another partner,
    and it will not propose the proper invoice in the reconciliation widget,
    even if it uses an exact match on the payment reference number.
    
    Having a parameter allowing to skip creation of the bank account in Odoo
    will allow the reconciliation to be based striclty on the reference number.
    
    closes odoo/odoo#168029
    
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models, _
 from odoo.osv import expression
 from odoo.tools import float_is_zero
 from odoo.tools import float_compare, float_round, float_repr
-from odoo.tools.misc import formatLang, format_date
+from odoo.tools.misc import formatLang, format_date, str2bool
 from odoo.exceptions import UserError, ValidationError
 
 import time
@@ -1310,7 +1310,9 @@ class AccountBankStatementLine(models.Model):
     def _find_or_create_bank_account(self):
         bank_account = self.env['res.partner.bank'].search(
             [('company_id', '=', self.company_id.id), ('acc_number', '=', self.account_number)])
-        if not bank_account:
+        if not bank_account and not str2bool(
+            self.env['ir.config_parameter'].sudo().get_param("account.skip_create_bank_account_on_reconcile")
+        ):
             bank_account = self.env['res.partner.bank'].create({
                 'acc_number': self.account_number,
                 'partner_id': self.partner_id.id,
