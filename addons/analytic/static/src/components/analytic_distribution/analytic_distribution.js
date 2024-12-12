e3cf8a97a6163e405dd53ddb9b43c2aa8c539f1d
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-12-12 14:14:39 +0100

    analytic: missing context in analytic_distribution widget
    Previously, the `context` attribute set in the `analytic_distribution` field was not usable in the related `AnalyticDistribution` widget. For example:
    ```xml
    <field name="analytic_distribution" widget="analytic_distribution"
           groups="analytic.group_analytic_accounting"
           optional="show"
           options="{'product_field': 'product_id', 'account_field': 'account_id', 'force_applicability': 'optional'}"
           context="{'context_key': 'context_value'}" <!-- not usable in the widget -->
    />
    ```
    The `context` was neither extracted nor propagated to relevant methods, causing issues in operations dependent on it.
    
    This fix ensures that the `context` is properly handled and propagated throughout the component, enabling its use in ORM interactions and improving the widget's functionality.
    
    This fix has been proposed to Odoo (https://github.com/odoo/odoo/pull/190510) and refused. Here we rebase this commit on top of the current `odoo/17.0` branch for submodule update purposes.

--- addons/analytic/static/src/components/analytic_distribution/analytic_distribution.js
+++ addons/analytic/static/src/components/analytic_distribution/analytic_distribution.js
@@ -1,5 +1,6 @@
 /** @odoo-module **/
 
+import { makeContext } from "@web/core/context";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { evaluateExpr } from "@web/core/py_js/py";
@@ -45,7 +46,12 @@ export class AnalyticDistribution extends Component {
         business_domain_compute: { type: String, optional: true },
         force_applicability: { type: String, optional: true },
         allow_save: { type: Boolean, optional: true },
+        context: { type: String, optional: true },
     }
+
+    static defaultProps = {
+        context: {},
+    };
 
     setup(){
         this.orm = useService("orm");
@@ -283,6 +289,7 @@ export class AnalyticDistribution extends Component {
                 },
                 // company domain might be required here
                 domain: [["root_plan_id", "=", account.planId]],
+                context: JSON.stringify(this.context),
             };
             values[fieldName] = account?.accountId
                 ? [account.accountId, account.accountDisplayName]
@@ -349,6 +356,7 @@ export class AnalyticDistribution extends Component {
         if (record.data.company_id) {
             args['company_id'] = record.data.company_id[0];
         }
+        args['context'] = this.context;
         return args;
     }
 
@@ -361,10 +369,12 @@ export class AnalyticDistribution extends Component {
         const args = {
             domain: domain,
             fields: ["id", "display_name", "root_plan_id", "color"],
-            context: [],
+            context: this.context,
         }
         // batched call
-        const records = await this.batchedOrm.read("account.analytic.account", domain[0][2], args.fields, {});
+        const records = await this.batchedOrm.read("account.analytic.account", domain[0][2], args.fields, {
+            context: args.context,
+        });
         return Object.assign({}, ...records.map((r) => {
             const {id, ...rest} = r;
             return {[id]: rest};
@@ -395,6 +405,14 @@ export class AnalyticDistribution extends Component {
     }
 
     // Getters
+    get context() {
+        const { context, record } = this.props;
+        const evalContext = record?.getEvalContext
+            ? record.getEvalContext(false)
+            : record?.evalContext;
+        return makeContext([context], evalContext)
+    }
+
     get valueColumnEnabled() {
         return Boolean(this.props.amount_field && this.props.record.data[this.props.amount_field]);
     }
@@ -680,7 +698,7 @@ export const analyticDistribution = {
             availableTypes: ["many2one"],
         }
     ],
-    extractProps: ({ attrs, options }) => ({
+    extractProps: ({ attrs, context, options }) => ({
         business_domain: options.business_domain,
         account_field: options.account_field,
         product_field: options.product_field,
@@ -688,6 +706,7 @@ export const analyticDistribution = {
         business_domain_compute: attrs.business_domain_compute,
         force_applicability: options.force_applicability,
         allow_save: !options.disable_save,
+        context: context,
     }),
 };
 
